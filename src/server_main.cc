6435b62dcfe84ca3986e0ec18f03b0228d39a855
Minhao <mren1423@g.ucla.edu>
2024-04-12 07:12:11 +0000

    commited the main file
    Change-Id: Idcfbd3295e6237b8c994ba9c386527c5cac67422

--- src/server_main.cc
+++ src/server_main.cc
@@ -13,7 +13,7 @@
 #include <boost/asio.hpp>
 #include "server.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
   try
   {
@@ -23,14 +23,15 @@ int main(int argc, char* argv[])
       return 1;
     }
 
-    boost::asio::io_service io_service;
+    boost::asio::io_service io_service; // Create an io_service object which provides I/O services, like sockets
 
-    using namespace std; // For atoi.
-    server s(io_service, atoi(argv[1]));
+    using namespace std;                 // For atoi.
+    server s(io_service, atoi(argv[1])); // Create an instance of the server class, initializing it with the I/O service and the port number to listen on
 
-    io_service.run();
+    io_service.run(); // Run the io_service object to perform asynchronous network operations
   }
-  catch (std::exception& e)
+  // if ant exception is thrown, catch it and print an error message
+  catch (std::exception &e)
   {
     std::cerr << "Exception: " << e.what() << "\n";
   }
