2c55c1ae58009248c56a499d9c9704a40e0cfeb0
Minhao <mren1423@g.ucla.edu>
2024-04-12 07:29:18 +0000

    comment the session.cc code
    Change-Id: If1d8bc34fd37fe6a02db255f55dc15c484e614fd

--- src/session.cc
+++ src/session.cc
@@ -3,26 +3,32 @@
 #include <boost/bind/bind.hpp>
 using namespace boost::placeholders;
 
-session::session(boost::asio::io_service& io_service)
-        : socket_(io_service)
+// Constructor for the 'session' class
+session::session(boost::asio::io_service &io_service)
+    : socket_(io_service) // Initialize the socket with the provided io_service
 {
 }
 
-tcp::socket& session::socket()
+// get a reference to the socket associated with this session
+tcp::socket &session::socket()
 {
     return socket_;
 }
 
+// start the session by reading from the socket
 void session::start()
 {
+    // Begin asynchronous read operation on the socket to read data into the buffer 'data_'
     socket_.async_read_some(boost::asio::buffer(data_, max_length),
                             boost::bind(&session::handle_read, this,
                                         boost::asio::placeholders::error,
                                         boost::asio::placeholders::bytes_transferred));
 }
 
-void session::handle_read(const boost::system::error_code& error, size_t bytes_transferred)
+// handle the completion of a read operation
+void session::handle_read(const boost::system::error_code &error, size_t bytes_transferred)
 {
+    // If there was no error, write the data back to the client
     if (!error)
     {
         boost::asio::async_write(socket_,
@@ -36,10 +42,13 @@ void session::handle_read(const boost::system::error_code& error, size_t bytes_t
     }
 }
 
-void session::handle_write(const boost::system::error_code& error)
+// handle the completion of a write operation
+void session::handle_write(const boost::system::error_code &error)
 {
+    // Check if the write operation completed successfully
     if (!error)
     {
+        // If successful, start another read operation to continue receiving data from the client
         socket_.async_read_some(boost::asio::buffer(data_, max_length),
                                 boost::bind(&session::handle_read, this,
                                             boost::asio::placeholders::error,
