b570bccb37babb9d0b0d85e9b741429459cffb80
Minhao <mren1423@g.ucla.edu>
2024-04-21 22:17:54 +0000

    ready to write unit tests
    Change-Id: I1d815577f966031b3a72f39909497995996afbf8

--- tests/config_parser_test.cc
+++ tests/config_parser_test.cc
@@ -14,6 +14,7 @@ TEST_F(NginxConfigParserTest, SimpleConfig) {
   EXPECT_TRUE(success);
 }
 
+//test for the case when the file does not exist
 TEST_F(NginxConfigParserTest, OpenNonExistentFile) {
 
   bool success = parser.Parse("none_config", &out_config);
@@ -21,6 +22,7 @@ TEST_F(NginxConfigParserTest, OpenNonExistentFile) {
   EXPECT_FALSE(success);
 }
 
+//test for the case when the file is empty
 TEST_F(NginxConfigParserTest, EmptyConfig) {
 
   bool success = parser.Parse("empty_config", &out_config);
@@ -28,6 +30,7 @@ TEST_F(NginxConfigParserTest, EmptyConfig) {
   EXPECT_TRUE(success);
 }
 
+//test when missing left bracket
 TEST_F(NginxConfigParserTest, MissLeftBracket) {
 
   bool success = parser.Parse("miss_left_brac", &out_config);
@@ -35,6 +38,7 @@ TEST_F(NginxConfigParserTest, MissLeftBracket) {
   EXPECT_FALSE(success);
 }
 
+//test when missing right bracket
 TEST_F(NginxConfigParserTest, MissRightBracket) {
 
   bool success = parser.Parse("miss_right_brac", &out_config);
@@ -42,6 +46,7 @@ TEST_F(NginxConfigParserTest, MissRightBracket) {
   EXPECT_FALSE(success);
 }
 
+//test when extra left bracket
 TEST_F(NginxConfigParserTest, ExtraLeftBracket) {
 
   bool success = parser.Parse("extra_left_brac", &out_config);
@@ -49,9 +54,11 @@ TEST_F(NginxConfigParserTest, ExtraLeftBracket) {
   EXPECT_FALSE(success);
 }
 
+//test when extra right bracket
 TEST_F(NginxConfigParserTest, ExtraRightBracket) {
 
   bool success = parser.Parse("extra_right_brac", &out_config);
 
   EXPECT_FALSE(success);
 }
+
