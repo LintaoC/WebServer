3f8d0cda7197cea567636b121c2d2199099aec48
Lintao Cui <lintao@g.ucla.edu>
2024-04-22 08:28:36 +0000

    Added first integration test
    Change-Id: I63e1587156d293ba503a0408a300bcb75e8103ee

---
+++ tests/integration_test.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+CONFIG_FILE="../simple_config"
+RESPONSE_PATH="../tests"
+SERVER_BIN="../build/bin/server"
+
+# Start the server in the background
+$SERVER_BIN $CONFIG_FILE &
+SERVER_PID=$!
+echo "Server PID: $SERVER_PID"
+
+# Allow server some time to start
+sleep 1
+
+# Perform a test request and capture the response
+RESPONSE=$(echo -ne "POST /submit-form HTTP/1.1\r\nHost: localhost\r\nContent-Type: application/x-www-form-urlencoded\r\nContent-Length: 27\r\n\r\nusername=johndoe&password=1234" | nc localhost 8080)
+CLEANED_RESPONSE=$(echo "$RESPONSE" | sed '/^Date:/d')
+
+# Normalize and save the actual response
+echo "$CLEANED_RESPONSE" | tr -d '\r' > test_response1
+
+# Check the response
+echo -n "Test 1   "
+if diff --ignore-trailing-space "${RESPONSE_PATH}/expected_response1" "test_response1"; then
+    echo "success"
+else
+    echo "failed"
+fi
+
+# Kill the server process and exit with the status of the diff command
+kill $SERVER_PID
+rm -f test_response1
+exit $?
+
+
